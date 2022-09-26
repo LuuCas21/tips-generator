1cdf021ada4f8e68bc0e4280d680f8ad54c269c3
Lucas R. de Souza <LucasRSouza2000@outlook.com>
2022-09-25 22:02:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use script';
 
-const tip_generator = function() {
+const aviso_mensagem = document.querySelector('.aviso');
+const quote_paragraph = document.querySelector('.quote');
+const copiar_mensagem = document.querySelector('.copiar');
+
+
+/*const tip_generator = function() {
 
 fetch('https://api.adviceslip.com/advice')
 .then(responseJson => {
@@ -14,4 +19,41 @@ fetch('https://api.adviceslip.com/advice')
 
 };
 
+tip_generator();*/
+
+const tip_generator = async () => {
+    const url = await fetch('https://api.adviceslip.com/advice');
+    const advice_data = await url.json();
+
+    quote_paragraph.innerHTML = advice_data.slip.advice;
+
+    quote_paragraph.setAttribute('data-advice', advice_data.slip.advice);
+
+    console.log(quote_paragraph.dataset.advice);
+
+    if (copiar_mensagem.style.visibility === 'visible') {
+        copiar_mensagem.style.visibility = 'hidden';
+        copiar_mensagem.style.opacity = '0';
+    };
+};
+
 tip_generator();
+
+function hoverAviso(visibility, opacity) {
+    aviso_mensagem.style.visibility = visibility;
+    aviso_mensagem.style.opacity = opacity;
+};
+
+function copiarMensagem() {
+    navigator.clipboard.writeText(this.getAttribute('data-advice'));
+
+    copiar_mensagem.style.visibility = 'visible';
+    copiar_mensagem.style.opacity = '0.8';
+};
+
+quote_paragraph.addEventListener('mouseover', hoverAviso.bind(this, 'visible','0.8'));
+
+quote_paragraph.addEventListener('mouseout', hoverAviso.bind(this, 'hidden', '0'));
+
+
+quote_paragraph.addEventListener('click', copiarMensagem);
